caa3bb88f18a96fd42e3cb01d11f1a5144224a1d
Dev Jangid <devsthr123@gmail.com>
2025-04-13 15:31:20 +0530

    firts commit

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -3,11 +3,14 @@ import { Link } from "wouter";
 const Logo = () => {
   return (
     <Link href="/">
-      <img
-        src="https://i.ibb.co/WW73yyCn/Whats-App-Image-2025-03-28-at-2-02-20-PM-removebg-preview-2.png"
-        alt="Nainaland Deals Logo"
-        className="h-12 w-auto cursor-pointer"
-      />
+      <div className="flex items-center gap-2">
+        <img
+          src="https://i.ibb.co/WW73yyCn/Whats-App-Image-2025-03-28-at-2-02-20-PM-removebg-preview-2.png"
+          alt="Nainaland Deals Logo"
+          className="h-12 w-auto cursor-pointer"
+        />
+        <span className="text-xl font-semibold text-primary">Nainaland Deals</span>
+      </div>
     </Link>
   );
 };
